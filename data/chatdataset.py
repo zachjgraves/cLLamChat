b9ac66dc4188ecc71c2664245f0ae79e61f8ba7a
Zach Graves <zgraves@ufl.edu>
2024-08-04 17:51:40 -0700

    Modified datasets.ipynb and chatdataset.py to create indexed datasets

--- data/chatdataset.py
+++ data/chatdataset.py
@@ -6,7 +6,7 @@ import torch
 from datasets import load_dataset
 from torch.utils.data import Dataset
 
-# Create a chat datset class
+# Create a chat dataset class
 class ChatDataset(Dataset):
     def __init__(self,):
         super(ChatDataset, self).__init__()
@@ -14,6 +14,21 @@ class ChatDataset(Dataset):
         # Load datasets
         ds1 = load_dataset("yahma/alpaca-cleaned")
         ds2 = load_dataset("allenai/WildChat-1M")
+
+        # Combine 'instruction', 'input', 'output'
+        def combineText(example):
+            example["input"] = example['instruction'] + ' ' + example['input'] + ': ' + example['output']
+            return example
+        updatedDataset = ds1.map(combineText)
+
+        # Rename and and select text data
+        updatedDataset = updatedDataset.rename_column('input', 'text')
+        updatedDataset = updatedDataset.select_columns('text')
+
+        # Add index
+        indexedDataset = updatedDataset.map(lambda example, idx: {'idx': f'{idx}'}, with_indices=True)
+        
+        return indexedDataset
 
     def __len__(self):
         return Error
